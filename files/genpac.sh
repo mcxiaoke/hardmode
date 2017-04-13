c0699f03834df2f0c0d6c396c603b57457deab10
mcxiaoke <mcxiaoke@live.com>
2017-04-13 14:57:37 +0800

    add gfwlist lite txt and pac

---
+++ files/genpac.sh
@@ -0,0 +1,7 @@
+# @Author: mcxiaoke
+# @Date:   2017-04-13 14:44:18
+# @Last Modified by:   mcxiaoke
+# @Last Modified time: 2017-04-13 14:56:14
+# pip install gfwlist2pac
+gfwlist2pac -i sites.txt -p "SOCKS5 127.0.0.1:1080;" -f ss.pac
+gfwlist2pac -i sites.txt -p "PROXY 10.8.0.1:8118;HTTPS 10.8.0.1:8443;DIRECT" -f db.pac
